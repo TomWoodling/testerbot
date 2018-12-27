0c8429beaa0b98d8f5b988321241fb23bd23d1f9
Tom Woodling <t.woodling@coolblue.nl>
2018-12-27 15:24:50 +0100

    Initial files added

---
+++ Base_mod/Test-Regex.psm1
@@ -0,0 +1,26 @@
+function Test-Regex {
+    <#
+    .SYNOPSIS
+        Displays a Grafana graph for a given type for a server.
+    .EXAMPLE
+        grafana cpu server01
+    .EXAMPLE
+        grafana disk myotherserver02
+    #>
+    [PoshBot.BotCommand(
+        Command = $false,
+        CommandName = 'Test-Regex',
+        TriggerType = 'regex',
+        Regex = "%"
+    )]
+    [cmdletbinding()]
+    param(
+        [parameter(ValueFromRemainingArguments = $true)]
+        [object[]]$Arguments
+    )
+
+    $first = $Arguments[1]
+    $second = $Arguments[2]
+
+    Return "I get $first for the first part and $second for the second - from $arguments"
+}
