f3d189f7a47b3fb108150b8339c77192cc808790
Tom Woodling <t.woodling@coolblue.nl>
2019-01-16 13:47:18 +0100

    help fixes

--- Base_mod/Get-RegexHelp.psm1
+++ Base_mod/Get-RegexHelp.psm1
@@ -20,36 +20,36 @@ function Get-RegexHelp {
         [object[]]$Arguments
     )
     $base = ":adbot: ADbot attempts to identify commands using regex matches - try following these examples:
-1. `adbot get groups for t.woodling` - gets all groups t.woodling is member of
-2. `adbot get users in gl-it-services` - gets all members of gl-it-services
-3. `adbot get groups in gl-it-services` - gets only nested groups in gl-it-services
-4. `adbot reports to t.woodling` - gets any users reporting to t.woodling (if there are any)
-5. `adbot search AD t-serv` - returns any group that contains the string 't-serv'
+1. adbot get groups for t.woodling - gets all groups t.woodling is member of
+2. adbot get users in gl-it-services - gets all members of gl-it-services
+3. adbot get groups in gl-it-services - gets only nested groups in gl-it-services
+4. adbot reports to t.woodling - gets any users reporting to t.woodling (if there are any)
+5. adbot search AD t-serv - returns any group that contains the string 't-serv'
 *Important note* quotes are no longer required for group names containing whitespace or special characters!
-*Further Note* Any old commands you used with adbot will still work, but you should replace the `adbot` with `!`
+*Further Note* Any old commands you used with adbot will still work, but you should replace the adbot with !
 
 "
     $usser = "User query examples:
-1. `adbot get groups for t.woodling` - gets all groups t.woodling is member of.
+1. adbot get groups for t.woodling - gets all groups t.woodling is member of.
 Alternate command syntax:
- i) `adbot membership t.woodling` 
- ii) `! groups4user t.woodling`
-2. `adbot reports to t.woodling` - gets any users reporting to t.woodling (if there are any)
+ i) adbot membership t.woodling 
+ ii) ! groups4user t.woodling
+2. adbot reports to t.woodling - gets any users reporting to t.woodling (if there are any)
 Altername command syntax:
- i) `adbot get direct reports for user t.woodling`
- ii) `! reports-to t.woodling`
+ i) adbot get direct reports for user t.woodling
+ ii) ! reports-to t.woodling
 
 "
 
     $grup = "Group query examples:
-1. `adbot get users in gl-it-services` - gets all members of gl-it-services
+1. adbot get users in gl-it-services - gets all members of gl-it-services
 Alternate command syntax:
- i) `adbot get members of group gl-it-services'
- ii) `! adgroup gl-it-services`
-2. `adbot get groups in gl-it-services` - gets only nested groups in gl-it-services
+ i) adbot get members of group gl-it-services'
+ ii) ! adgroup gl-it-services
+2. adbot get groups in gl-it-services - gets only nested groups in gl-it-services
 Altername command syntax:
- i) `adbot groups in gl-it-services`
- ii) `! groups-in gl-it-services"
+ i) adbot groups in gl-it-services
+ ii) ! groups-in gl-it-services"
 
     if ($Arguments) {
         if ($Arguments[5] -match 'user' -and $Arguments[5] -notmatch 'group') {$hulp = $base+$usser}
