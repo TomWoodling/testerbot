6da57823a1fdc04103787aeba7184fa62f6f824f
Tom Woodling <t.woodling@coolblue.nl>
2019-04-16 17:06:54 +0200

    dadjokes

---
+++ Base_mod/Get-Dadjoke.psm1
@@ -0,0 +1,26 @@
+﻿function Get-Dadjoke {
+    [PoshBot.BotCommand(
+        Command = $false,
+        CommandName = 'Get-Dadjoke',
+        TriggerType = 'regex',
+        Regex = "%"
+    )]
+    [cmdletbinding()]
+    param(
+        $Bot,
+        [parameter(ValueFromRemainingArguments = $true)]
+        [object[]]$Arguments
+    )
+
+    if ($Arguments) {
+        $headers = @{
+        'Accept'="application/json"
+        }
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    $term = $arguments[4]
+    $lulz = ((irm -uri 'https://icanhazdadjoke.com/search?term=dogs&limit=10' -Method Get -Headers $headers | select -ExpandProperty results) | Get-Random) | select -ExpandProperty joke
+        }
+    else {$lulz = (irm -uri https://icanhazdadjoke.com/slack -Method Get).attachments.text}
+
+    Return $lulz
+}
